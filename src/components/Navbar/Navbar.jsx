0e894e76fdc82531b28026e02f29752517633d7d
Sumit Tripathi <sumit.tpt07@gmail.com>
2024-01-09 20:17:23 +0530

    Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
         <Link to="/" className="deco-none">
           <Navbar.Brand to="/">
             <ion-icon name="bag"></ion-icon>
-            <h1 className="logo">Multimart</h1>
+            <h1 className="logo">Arch EyeWare</h1>
           </Navbar.Brand>
         </Link>
         {/* Media cart and toggle */}
