e068c0080d7f818be9824037c35aba92f0439b15
Sumit Tripathi <sumit.tpt07@gmail.com>
2024-01-16 23:08:14 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <Col md={3} sm={5} className="box">
             <div className="logo">
               <ion-icon name="bag"></ion-icon>
-              <h1>Multimart</h1>
+              <h1>Arch EyeWare</h1>
             </div>
             <p>Get your favourate eye at MultiMart at very hood price</p>
           </Col>
