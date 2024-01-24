a101bf0576e2668851810eb9bdf788875fbf8668
Sumit Tripathi <sumit.tpt07@gmail.com>
2024-01-24 22:46:51 +0530

    empty

--- src/utils/products.js
+++ src/utils/products.js
@@ -266,7 +266,7 @@ export const products = [
     category: "all",
     price: 173,
     shortDesc:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo!",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
     reviews: [
