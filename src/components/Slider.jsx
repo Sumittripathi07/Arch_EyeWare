11878825951c8587a6c3cc468fccf4d3d8d33a83
Sumit <sumit.tpt07@gmail.com>
2024-07-18 01:41:10 +0530

    empty

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -19,8 +19,11 @@ const SliderHome = () => {
           <Slider {...settings}>
           {SliderData.map((value, index) => {
             return (
+            
               <SlideCard key={index} title={value.title} cover={value.cover} desc={value.desc} />
+            
             )
+
           })}
         </Slider>
         </Container>
